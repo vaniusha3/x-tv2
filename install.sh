253b3d7cdd1de75067a97b252a335212696a0a9d
vaniusha3 <dj_vaniusha@mail.ru>
2017-07-03 17:14:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 #
-apt-get update && apt-get upgrade -y
-apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip -y && apt-get install php5-mcrypt &&  php5enmod mcrypt
-service apache2 restart
-wget http://maxdata755.online/public/xt_1.0.60/installer/www_dir.tar.gz -O /tmp/www_dir.tar.gz
-#
-if [ -d /var/www/html ];
-then
-    echo "/var/www/html/ exists."
-	tar -zxvf /tmp/www_dir.tar.gz -C /var/www/html/
-else
-    echo " "
-	if [ -d /root/www/ ];
-	then
-    echo "/var/www/ exist"
-    	tar -zxvf /tmp/www_dir.tar.gz -C /var/www/
-else
-    echo "No Webserver installed?"
-
-fi
-
-fi
-#
-wget http://127.0.0.1/downloads/iptv_panel_pro.zip -O /tmp/iptv_panel_pro.zip
-wget http://127.0.0.1/downloads/install_iptv_pro.zip && unzip install_iptv_pro.zip
-#
 php install_iptv_pro.php
 #
 echo " "
